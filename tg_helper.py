a20e61ec50158446fa1a43a02e727fbb2c8fe14e
MarArek <arek.maruszczak@gmail.com>
2020-01-02 13:46:28 +0000

    Fix in tg helper

--- tg_helper.py
+++ tg_helper.py
@@ -6,6 +6,9 @@ TG_USER_ID = getenv("TG_USER_ID")
 TG_URL = f"https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage"
 
 def send(text, parse_mode="html"):
+	if TG_USER_ID is None or TG_BOT_TOKEN is None:
+		print("Telegram tokens are not set")
+		exit(0)
 	data = {
 		"chat_id": int(TG_USER_ID),
 		"text": text,
